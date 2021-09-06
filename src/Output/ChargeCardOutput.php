698e455a7bdb7fa67121e4ae474607defce11c94
Noor Adiana <me@nooradiana.com>
2021-09-07 00:03:56 +0700

    Implement charge method

---
+++ src/Output/ChargeCardOutput.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ziswapp\Payment\Output;
+
+use DateTimeInterface;
+
+final class ChargeCardOutput extends TransactionOutput
+{
+    private ?string $redirectUrl = null;
+
+    private string $fraudStatus;
+
+    private DateTimeInterface $transactionDate;
+
+    public static function create(
+        string $transactionId,
+        string $orderId,
+        string $status,
+        string $fraudStatus,
+        float $amount,
+        DateTimeInterface $transactionDate,
+        ?string $redirectUrl = null,
+        array $originalOutput = []
+    ): self {
+        $self = new self();
+
+        $self->transactionId = $transactionId;
+        $self->orderId = $orderId;
+        $self->status = $status;
+        $self->fraudStatus = $fraudStatus;
+        $self->amount = $amount;
+        $self->redirectUrl = $redirectUrl;
+        $self->transactionDate = $transactionDate;
+        $self->originalOutput = $originalOutput;
+
+        return $self;
+    }
+
+    public function getRedirectUrl(): ?string
+    {
+        return $this->redirectUrl;
+    }
+
+    public function getFraudStatus(): string
+    {
+        return $this->fraudStatus;
+    }
+
+    public function getTransactionDate(): DateTimeInterface
+    {
+        return $this->transactionDate;
+    }
+}
