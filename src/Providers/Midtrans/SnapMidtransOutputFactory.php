5b203d053538473474a9f342137283c4ccfb7f15
Noor Adiana <me@nooradiana.com>
2021-09-07 08:26:34 +0700

    Add redirect input and remove return type hint

--- src/Providers/Midtrans/SnapMidtransOutputFactory.php
+++ src/Providers/Midtrans/SnapMidtransOutputFactory.php
@@ -5,15 +5,14 @@ declare(strict_types=1);
 namespace FromHome\Payment\Providers\Midtrans;
 
 use Psl\Type;
-use FromHome\Payment\Output\CStoreOutput;
-use FromHome\Payment\Output\EWalletOutput;
 use FromHome\Payment\Output\CheckStatusOutput;
 use FromHome\Payment\Output\CardBinFilterOutput;
-use FromHome\Payment\Output\VirtualAccountOutput;
 use FromHome\Payment\Contracts\OutputFactoryInterface;
+use FromHome\Payment\Output\RedirectTransactionOutput;
+use FromHome\Payment\Contracts\RedirectOutputFactoryInterface;
 use FromHome\Payment\Exceptions\MethodNotImplementedException;
 
-final class SnapMidtransOutputFactory implements OutputFactoryInterface
+final class SnapMidtransOutputFactory implements OutputFactoryInterface, RedirectOutputFactoryInterface
 {
     private function __construct()
     {
@@ -29,55 +28,43 @@ final class SnapMidtransOutputFactory implements OutputFactoryInterface
         return MidtransOutputFactory::create()->fromStatusArray($data);
     }
 
-    public function fromVirtualAccountArray(array $data): VirtualAccountOutput
+    public function fromVirtualAccountArray(array $data): RedirectTransactionOutput
     {
         $data = Type\shape([
             'token' => Type\non_empty_string(),
             'redirect_url' => Type\non_empty_string(),
         ])->coerce($data);
 
-        return VirtualAccountOutput::create(
+        return RedirectTransactionOutput::create(
+            $data['token'],
             $data['redirect_url'],
-            'snap',
-            null,
-            null,
-            null,
             $data
         );
     }
 
-    public function fromCStoreArray(array $data): CStoreOutput
+    public function fromCStoreArray(array $data): RedirectTransactionOutput
     {
         $data = Type\shape([
             'token' => Type\non_empty_string(),
             'redirect_url' => Type\non_empty_string(),
         ])->coerce($data);
 
-        return CStoreOutput::create(
+        return RedirectTransactionOutput::create(
+            $data['token'],
             $data['redirect_url'],
-            'snap',
-            null,
-            null,
-            null,
             $data
         );
     }
 
-    public function fromEWalletArray(array $data): EWalletOutput
+    public function fromEWalletArray(array $data): RedirectTransactionOutput
     {
         $data = Type\shape([
             'token' => Type\non_empty_string(),
             'redirect_url' => Type\non_empty_string(),
         ])->coerce($data);
 
-        return EWalletOutput::create(
-            'snap',
-            null,
-            null,
-            null,
-            $data['redirect_url'],
-            null,
-            $data['redirect_url'],
+        return RedirectTransactionOutput::create(
+            $data['token'],
             $data['redirect_url'],
             $data
         );
@@ -89,5 +76,19 @@ final class SnapMidtransOutputFactory implements OutputFactoryInterface
     public function fromFilterBinArray(array $data): CardBinFilterOutput
     {
         throw new MethodNotImplementedException(sprintf('This method `%s` not implemented', __FUNCTION__));
+    }
+
+    public function fromRedirectArray(array $data): RedirectTransactionOutput
+    {
+        $data = Type\shape([
+            'token' => Type\non_empty_string(),
+            'redirect_url' => Type\non_empty_string(),
+        ])->coerce($data);
+
+        return RedirectTransactionOutput::create(
+            $data['token'],
+            $data['redirect_url'],
+            $data
+        );
     }
 }
